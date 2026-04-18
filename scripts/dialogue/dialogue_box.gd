ad117dd15aab12e9df59e2e210d706788eff0c6d
Gababe <gg.godfreygabriel@gmail.com>
2026-04-18 10:42:49 -0400

    added text effects

--- scripts/dialogue/dialogue_box.gd
+++ scripts/dialogue/dialogue_box.gd
@@ -3,9 +3,10 @@ extends CanvasLayer
 @onready var text_label: RichTextLabel = $RichTextLabel
 
 func starting_dialogue():
+	text_label.visible = true
 	var read_dialogue = false
 	var dialogue_loop = {
-		line0 = "room0_string0",
+		line0 = "[wave amp=50.0 freq=5.0 connected=1]room0_string0[/wave]",
 		line1 = "room0_string1"
 	}
 
@@ -14,6 +15,7 @@ func starting_dialogue():
 			text_label.text = line
 			await get_tree().create_timer(2.0).timeout
 		read_dialogue = true
+		text_label.visible = false
 
 func _ready():
 	starting_dialogue()
