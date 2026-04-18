d95302290ff68093a432e2117a47a66cc53b2c95
Gababe <gg.godfreygabriel@gmail.com>
2026-04-17 21:52:46 -0400

    adding sprite flipping logic

--- scripts/player/player_character.gd
+++ scripts/player/player_character.gd
@@ -4,10 +4,18 @@ extends CharacterBody2D
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 
+@onready var sprite: AnimatedSprite2D = $AnimatedSprite2D
 
 func get_input():
 	var input = Input.get_vector("a", "d", "w", "s")
 	velocity = input * SPEED
+
+	if velocity.x < 0:
+		sprite.flip_h = true
+		print("Moving left")
+	else:
+		sprite.flip_h = false
+		print("Moving right")
 
 func _physics_process(delta):
 	get_input()
