ae515cea86683af9b98bade4c8c1e65f7c112677
wtv <wtv@protonmail.ch>
2026-04-18 17:12:06 -0400

    dialog events and queue

--- scripts/EventBus.gd
+++ scripts/EventBus.gd
@@ -1,3 +1,5 @@
 extends Node
 
 signal task_completed(task_id)
+signal display_dialog(dialog_string_id)
+signal queue_dialog(dialog_string_id)
--- scripts/dialogue/dialogue_box.gd
+++ scripts/dialogue/dialogue_box.gd
@@ -4,22 +4,40 @@ extends CanvasLayer
 @onready var text_panel: MarginContainer = $MarginContainer
 @onready var text_box: NinePatchRect = $MarginContainer/NinePatchRect
 
-func starting_dialogue():
+
+var dialogue_queue = []
+var display_dialog_timer
+
+func _on_queue_dialog(dialog_string_id):
+	if display_dialog_timer.is_stopped():
+		start_displaying_dialog(dialog_string_id)
+	else:
+		dialogue_queue.append(dialog_string_id)
+
+func _on_display_dialog(dialog_string_id):
+	start_displaying_dialog(dialog_string_id)
+
+func start_displaying_dialog(dialog_string_id):
 	text_panel.visible = true
 	text.visible = true
-	var read_dialogue = false
-	var dialogue_loop = {
-		line0 = "room0_string0",
-		line1 = "room0_string1"
-	}
+	text.text = dialog_string_id
+	display_dialog_timer.start()
 
-	if not read_dialogue:
-		for line in dialogue_loop.values():
-			text.text = line
-			await get_tree().create_timer(2.0).timeout
-		read_dialogue = true
-		text.visible = false
-		text_panel.visible = false
+func display_next_dialog():
+	text.visible = false
+	text_panel.visible = false
+
+	if not dialogue_queue.is_empty():
+		start_displaying_dialog(dialogue_queue.pop_front())
 
 func _ready():
-	starting_dialogue()
+	display_dialog_timer = Timer.new()
+	display_dialog_timer.autostart = false
+	display_dialog_timer.one_shot = true
+	display_dialog_timer.wait_time = 2.0
+	display_dialog_timer.timeout.connect(display_next_dialog)
+	add_child(display_dialog_timer)
+	EventBus.queue_dialog.connect(_on_queue_dialog)
+	EventBus.display_dialog.connect(_on_display_dialog)
+	EventBus.queue_dialog.emit("room0_string0")
+	EventBus.queue_dialog.emit("room0_string1")
