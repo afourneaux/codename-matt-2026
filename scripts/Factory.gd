eef0487123d497592374a493b135d91727c65ded
Alex Fourneaux <Alex@fourneaux.ca>
2026-04-18 10:30:36 -0400

    Move betwixt rooms

--- scripts/Factory.gd
+++ scripts/Factory.gd
@@ -3,6 +3,15 @@ extends Object
 
 
 static var room_obj = preload("res://scenes/rooms/room.tscn")
+static var character_obj = preload("res://scenes/player/player_character.tscn")
+const ROOM_CENTRE = Vector2(576,450)
+
+static var door_positions = {
+	0: Vector2(450, 400),
+	1: Vector2(700, 400),
+	2: Vector2(700, 500),
+	3: Vector2(450, 500)
+}
 
 static var room_data = {
 	# Room 0 - beginning / end room
@@ -30,13 +39,20 @@ static var room_data = {
 }
 
 
-static func create_room(room_id: int):
+static func create_room(room_id: int, last_door_position: int):
 	var data = room_data[room_id]
 	var room = room_obj.instantiate()
 	room.room_id = room_id
 	room.image = data["image"]
+	var new_door_position = (last_door_position + 2) % 4
 	for link in data["links"]:
-		room.create_link(link, data["links"][link])
+		room.create_link(link, door_positions[link], data["links"][link], link != new_door_position)
 	for interaction in data["interactions"]:
 		room.create_interaction(interaction["posx"], interaction["posy"], interaction["id"])
+	var character = character_obj.instantiate()
+	if last_door_position == -1:
+		character.position = ROOM_CENTRE
+	else:
+		character.position = door_positions[new_door_position]
+	room.add_child(character)
 	return room
